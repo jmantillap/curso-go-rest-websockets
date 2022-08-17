8c059e1f28dc432d4fff59d8f77e9478a8bfd73b
Javier Orlando Mantilla Portilla <jmantilla@intergrupo.com>
2022-08-17 13:26:59 -0500

    Creando el registro de usuarios.

--- repository/user.go
+++ repository/user.go
@@ -9,7 +9,8 @@ import (
 type UserRepository interface {
 	InsertUser(ctx context.Context, user *models.User) error
 	GetUserByID(ctx context.Context, id int64) (*models.User, error)
-	UpdateUser(ctx context.Context, user *models.User) error
+	//UpdateUser(ctx context.Context, user *models.User) error
+	Close() error
 }
 
 var implementation UserRepository
@@ -26,6 +27,11 @@ func GetUserByID(ctx context.Context, id int64) (*models.User, error) {
 	return implementation.GetUserByID(ctx, id)
 }
 
+/*
 func UpdateUser(ctx context.Context, user *models.User) error {
 	return implementation.UpdateUser(ctx, user)
+}
+*/
+func Close() error {
+	return implementation.Close()
 }
